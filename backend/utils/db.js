f96051d5b868618490e391664dbc1cbd9e221b73
Riad <riadislam9117@gmail.com>
2026-02-10 19:05:26 +0600

    Fixed DB connection

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,11 +1,11 @@
-import dns from "dns";
 import mongoose from "mongoose";
-
-dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Database connected...");
   } catch (error) {
     console.log(error.message);
